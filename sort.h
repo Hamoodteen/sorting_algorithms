f31e2c64bd8f898bbde36971e464b235334e6753
Hamoodteen <ahmed104567@outlook.com>
2023-11-19 01:54:34 +0200

    0

--- sort.h
+++ sort.h
@@ -20,6 +20,18 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * swapint - f
+ * @a: a
+ * @b: b
+*/
+void swapint(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swapint(int *a, int *b);
