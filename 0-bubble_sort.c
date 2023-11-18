47e9ef1ed50c721d491ec2ae05dc317c5f722656
Hamoodteen <ahmed104567@outlook.com>
2023-11-18 22:05:56 +0200

    0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,6 +8,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
+
 	if (size < 2)
 	{
 		print_array(array, size);
@@ -15,8 +16,6 @@ void bubble_sort(int *array, size_t size)
 	}
 	for (i = 0; i < size - 1; i++)
 	{
-		if (array[i] > array[i + 1])
-			swapint(&array[i], &array[i + 1]);
-		print_array(array, size);
+		return;
 	}
 }
