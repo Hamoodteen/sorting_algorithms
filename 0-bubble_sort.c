0e48a48d5ef1d4a2a12acab8a9c1a307f430317f
Hamoodteen <ahmed104567@outlook.com>
2023-11-18 22:01:42 +0200

    0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,6 +7,18 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	(void)array;
-	(void)size;
+	size_t i;
+	if (size < 2)
+	{
+		print_array(array, size);
+		return;
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		if (array[i] > array[i + 1])
+		{
+			swapint(&array[i], &array[i + 1]);
+			print_array(array, size);
+		}
+	}
 }
--- sort.h
+++ sort.h
@@ -21,6 +21,7 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swapint(int *a, int *b);
 void bubble_sort(int *array, size_t size);
 
 #endif
