05c20116fc8c9d5bd73506af670f3d5358ef9f74
Hamoodteen <ahmed104567@outlook.com>
2023-11-19 01:19:06 +0200

    0

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,21 +19,22 @@ void swapint(int *a, int *b)
 */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i = 0, j, mini;
 
 	if ((array == NULL) || (size == 0))
 		return;
-	for (i = 0; i < size - 1; i++)
+	mini = i;
+	for (j = i + 1; j < size; j++)
 	{
-		j = i;
-		while (j < size - 1)
+		if (array[j] < array[mini])
 		{
-			if (array[i] > array[j + 1])
-			{
-				swapint(&array[i], &array[j + 1]);
-				print_array(array, size);
-			}
-			j++;
+			mini = j;
 		}
+	}
+
+	if (mini != i)
+	{
+		swapint(&array[i], &array[mini]);
+		print_array(array, size);
 	}
 }
