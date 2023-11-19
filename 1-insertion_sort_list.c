c77fe15c44e36f95c5f8596da63bf8a0c3716ee2
Hamoodteen <ahmed104567@outlook.com>
2023-11-19 02:27:07 +0200

    0

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,33 +7,33 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *sorted = NULL;
-	listint_t *current = *list;
+	listint_t *curr = *list;
 	listint_t *temp, *prev, *ptr;
 
-	while (current != NULL)
+	while (curr != NULL)
 	{
-		temp = current->next;
+		temp = curr->next;
 		prev = NULL;
 		ptr = sorted;
-		while ((ptr != NULL) && (ptr->n <= current->n))
+		while ((ptr != NULL) && (ptr->n <= curr->n))
 		{
 			prev = ptr;
 			ptr = ptr->next;
 		}
 		if (prev == NULL)
 		{
-			current->prev = NULL;
-			sorted = current;
+			curr->prev = NULL;
+			sorted = curr;
 		}
 		else
 		{
-			prev->next = current;
-			current->prev = prev;
+			prev->next = curr;
+			curr->prev = prev;
 		}
-		current->next = ptr;
+		curr->next = ptr;
 		if (ptr != NULL)
-			ptr->prev = current;
-		current = temp;
+			ptr->prev = curr;
+		curr = temp;
 	}
 	*list = sorted;
 }
